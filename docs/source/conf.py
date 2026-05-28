e660c3835519ae5a6f07cb9104dfbdc31bb7c1a7
Tom Russell <tom.russell@ouce.ox.ac.uk>
2026-05-28 18:24:08 +0100

    Scaffold docs

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "transport_flow_model"
+copyright = "2026, Tom Russell, Raghav Pant, Yue Li, Alberto Fernández-Pérez"
+author = "Tom Russell, Raghav Pant, Yue Li, Alberto Fernández-Pérez"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
